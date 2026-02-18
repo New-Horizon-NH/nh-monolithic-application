baf6c3087e157f2a4db181a6a996c0fdabcfac73
pingmyheart <antoniorussi1972@gmail.com>
2026-02-18 16:16:25 +0100

    [UPDATE] - test

--- src/test/java/com/newhorizon/nhmonolithicapplication/service/DeviceServiceMockTest.java
+++ src/test/java/com/newhorizon/nhmonolithicapplication/service/DeviceServiceMockTest.java
@@ -29,7 +29,7 @@ import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
